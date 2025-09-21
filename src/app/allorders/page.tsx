7467161aa841370d992a2f7f244ef648943b0de0
MohamedAlgot <amiralgotali@gmail.com>
2025-09-21 22:33:11 +0300

    update payment

--- src/app/allorders/page.tsx
+++ src/app/allorders/page.tsx
@@ -4,8 +4,8 @@ import { Store } from "lucide-react";
 export default function AllOrders() {
   return (
     <>
-      <div className="container mx-auto text-center my-28 ">
-        <div className="flex justify-center items-center gap-3 ">
+      <div className="container mx-auto text-center my-28 mb-56 ">
+        <div className="flex justify-center items-center gap-3  ">
           {" "}
           <h2 className="text-green-500 font-bold text-6xl">
             Payment Made 
